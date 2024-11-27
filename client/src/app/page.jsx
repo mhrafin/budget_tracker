321fea5ce99a772ee2ecb5b1649c642b58d8be66
Cyanider <62058733+Hasan069@users.noreply.github.com>
2024-11-27 15:20:19 +0600

    taking the data from backend and showing it

---
+++ client/src/app/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setmessage] = useState("Hi Nigga");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/home");
+        const data = await response.json();
+        setmessage(data.message);
+      } catch (error) {
+        console.error("error fetching the data", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>{message}</div>;
+}
